dbee3b190ec080203123c89488f4e3847ce3837a
ilyaberbx <Ilyaberbx@users.noreply.github.com>
2023-10-02 14:34:36 +0300

    fix loading curtain

--- Assets/Scripts/Logic/LoadingCurtain.cs
+++ Assets/Scripts/Logic/LoadingCurtain.cs
@@ -1,28 +1,38 @@
-using System.Threading.Tasks;
+using System;
 using DG.Tweening;
 using UnityEngine;
-using Task = UnityEditor.VersionControl.Task;
 
 namespace Logic
 {
     public class LoadingCurtain : MonoBehaviour
     {
         [SerializeField] private CanvasGroup _canvasGroup;
+        private Guid _uid;
+
         private void Awake()
             => DontDestroyOnLoad(this);
 
         public Tween Show()
         {
+            StopPreviousTween();
+            CreateNewId();
+
             gameObject.SetActive(true);
             _canvasGroup.alpha = 0;
-            return _canvasGroup.DOFade(1, 3f)
-                .OnComplete(DisableObject);
+            return _canvasGroup.DOFade(1, 2f);
         }
-        public void Hide() =>
-            _canvasGroup.DOFade(0, 3f)
-                .OnComplete(DisableObject);
 
-        private void DisableObject() 
+        private void CreateNewId() 
+            => _uid = Guid.NewGuid();
+
+        private void StopPreviousTween() 
+            => DOTween.Kill(_uid);
+
+        public void Hide() =>
+            _canvasGroup.DOFade(0, 2f)
+                .OnComplete(DisableObject).SetId(_uid);
+
+        private void DisableObject()
             => gameObject.SetActive(false);
     }
 }
